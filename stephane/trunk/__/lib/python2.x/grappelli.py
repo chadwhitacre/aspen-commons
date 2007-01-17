79b577e8a7ee429f7abee38f6e3943642cd6eb30
whit537 <whit537@36fe2a08-3024-0410-92dd-e385f9db7a35>
2007-01-17 20:36:50 +0000

    honoring existing DJANGO_SETTINGS_MODULE; adding doc

--- stephane/trunk/__/lib/python2.x/grappelli.py
+++ stephane/trunk/__/lib/python2.x/grappelli.py
@@ -7,9 +7,21 @@ import aspen
 from django.core.handlers.wsgi import WSGIHandler
 
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'stephane.settings'
+DJANGO_SETTINGS_MODULE = 'stephane.settings' # change if you want
+
+
+# Tell Django which settings to use.
+# ==================================
+# If you don't like the default project name above, I don't blame you. Change
+# it, or set DJANGO_SETTINGS_MODULE explicitly in aspen's environment.
+
+if not os.environ.has_key('DJANGO_SETTINGS_MODULE'):
+    os.environ['DJANGO_SETTINGS_MODULE'] = DJANGO_SETTINGS_MODULE
 sys.path.insert(0, aspen.paths.root)
 
 
-django = WSGIHandler()
+# Instantiate Django's WSGI handler.
+# ==================================
+# This is then wired up to the site root in __/etc/apps.conf.
 
+django = WSGIHandler()
