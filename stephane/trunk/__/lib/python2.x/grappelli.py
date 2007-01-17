3a8feb7fbe56c9e50982f8570b8e79c23dc6e605
whit537 <whit537@36fe2a08-3024-0410-92dd-e385f9db7a35>
2007-01-17 03:34:49 +0000

    upgraded to Aspen 0.7

--- stephane/trunk/__/lib/python2.x/grappelli.py
+++ stephane/trunk/__/lib/python2.x/grappelli.py
@@ -3,13 +3,13 @@
 import os
 import sys
 
+import aspen
 from django.core.handlers.wsgi import WSGIHandler
 
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'stephane.settings'
+sys.path.insert(0, aspen.paths.root)
 
 
-class Django(WSGIHandler):
-    def __init__(self, config):
-        sys.path.insert(0, config.paths.root)
-        WSGIHandler.__init__(self)
+django = WSGIHandler()
+
